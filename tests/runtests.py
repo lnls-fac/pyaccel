8d4d7bfbdbc955da5fc22c38d78829411ad9347f
ximenes <xresende@gmail.com>
2015-04-15 08:31:44 -0300

    test_tracking.py
    - uncommented tests in the test suite that was mistakenly left commented

--- tests/runtests.py
+++ tests/runtests.py
@@ -9,8 +9,9 @@ import test_lattice
 suite_list = []
 suite_list.append(test_elements.get_suite())
 suite_list.append(test_accelerator.get_suite())
-suite_list.append(test_tracking.get_suite())
 suite_list.append(test_lattice.get_suite())
+suite_list.append(test_tracking.get_suite())
+
 
 tests = unittest.TestSuite(suite_list)
 unittest.TextTestRunner(verbosity=2).run(tests)
