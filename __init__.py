d99745be6c3df603c462dad78c8040ab792e2e65
ximenes <xresende@gmail.com>
2015-05-08 18:47:24 -0300

    Change in __init__.py (minor)

--- __init__.py
+++ __init__.py
@@ -1,11 +1,10 @@
 
-import os as _os
 from . import elements
 from . import accelerator
 from . import lattice
 from . import tracking
 from . import optics
 
-
+import os as _os
 with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
     __version__ = _f.read().strip()
