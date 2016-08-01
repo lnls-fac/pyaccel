97bbe70f794dca4648d1a6cc179fcd022ae1c38f
Fernando <fernandohds564@gmail.com>
2016-08-01 10:12:37 -0300

    Creation of NAFF module with general function for signal analysis.

--- pyaccel/__init__.py
+++ pyaccel/__init__.py
@@ -6,6 +6,7 @@ from . import tracking
 from . import optics
 from . import graphics
 from . import lifetime
+from . import naff
 
 import os as _os
 with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
