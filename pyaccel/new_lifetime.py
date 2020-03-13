2d657c132ffaff13065f51a926aec5f42dd9febf
Ana Luisa Ribeiro <analuisa@lnls560-linux.abtlus.org.br>
2020-03-13 11:18:07 -0300

    Update new_lifetime

--- pyaccel/new_lifetime.py
+++ pyaccel/new_lifetime.py
@@ -1,8 +1,9 @@
 import pyaccel
 import pymodels
+import mathphys
 
 import numpy as np
-from scipy import fixed_quad
+from scipy import integrate
 import math
 
 #Life time calculations
@@ -22,6 +23,7 @@ ring_loc = 0
 
 
 
+
 def lifetime(model, Vx, Vy, spread, P, I, n_bunches, emit, sig_S, acoplamento, accep):
     lattice = pymodels.si.lattice.create_lattice()
     #parameters
@@ -33,13 +35,13 @@ def lifetime(model, Vx, Vy, spread, P, I, n_bunches, emit, sig_S, acoplamento, a
     tau_am = pyaccel.optics.EquilibriumParameters.taue #radiationDamping;
     k =  pymodels.si.harmonic_number   #harmon;
     q = pyaccel.optics.EquilibriumParameters.overvoltage
-    qe = 1.60217662e-19; # electron charge [C]
-    r0 = 2.8179403227e-15; # classic electron radius [m]
-    c  = 2.99792458e8;  # speed of light [m/s]
-    Kb = 1.38e-23
+    qe = mathphys.constants.elementary_charge; # electron charge [C]
+    r0 = mathphys.constants.electron_radius; # classic electron radius [m]
+    c  = mathphys.constants.light_speed;  # speed of light [m/s]
+    Kb = mathphys.constants.boltzmann_constant
     T=300
-    epsilon0 = 8.854187817e-12; #Vacuum permittivity [F/m]
-    m_e = 511e3#eV
+    epsilon0 = mathphys.constants.vacuum_permitticity #Vacuum permittivity [F/m]
+    m_e = mathphys.constants.electron_rest_energy/qe#eV
     gamma = E0/m_e
    
 
@@ -51,10 +53,13 @@ def lifetime(model, Vx, Vy, spread, P, I, n_bunches, emit, sig_S, acoplamento, a
     By       = twiss.betay
     Ib = I/n_bunches
     T_rev    = pyaccel.optics.get_revolution_period(model)#s
-    sig      = pyaccel.optics.get_beam_size(model)
     eta_x    = twiss.etax
+    length   = pyaccel.lattice.get_attribute(model, 'length')
+    L        = sum(length)
+    Vac_x    = np.array(pyaccel.lattice.get_attribute(model, 'vmax'))-np.array(pyaccel.lattice.get_attribute(model, 'vmin'))
+    Vac_y    = np.array(pyaccel.lattice.get_attribute(model, 'hmax'))-np.array(pyaccel.lattice.get_attribute(model, 'hmin'))
     
-    tau_es = elastic(Vx, Vy, Bx, By, c, qe, epsilon0, Kb, Z, E0, T, P)/3600
+    tau_es = elastic(Vx, Vy, Bx, By, c, qe, epsilon0, Kb, Z, E0, T, P, length, L, Vac_x, Vac_y)/3600
 
     tau_is = inelastic(accep, P)/3600
 
@@ -64,24 +69,42 @@ def lifetime(model, Vx, Vy, spread, P, I, n_bunches, emit, sig_S, acoplamento, a
 
     return tau_es, tau_is, tau_T, tau 
 
-def elastic(Vx, Vy, Bx, By, c, qe, epsilon0, Kb, Z, E0, T, P):
+def elastic(Vx, Vy, Bx, By, c, qe, epsilon0, Kb, Z, E0, T, P, length, L, Vac_x, Vac_y):
+    #Calcular valores limite
+    Ex = Vac_x**2/Bx
+    Ey = Vac_y**2/By
+
+    Vac_xmin =  np.amin(Ex)
+    Vac_ymin =  np.amin(Ey)
+
+    #calcular valor scrapper
+    Scr_x = Vx**2 / Bx
+    Scr_y = Vy**2 / By
     
-    #calcular aceitancias
-    if (Vx<12e6):
-        EA_x = 1e6*Vx**2 / Bx
-    else:
-        EA_x = 12**2 / Bx
-    if (Vy<3e-3):
-        EA_y = 1e6*Vy**2 / By
-    else:
-        EA_y = 3**2 / By
+    EA_x = np.zeros(len(Bx))
+    EA_y = np.zeros(len(Bx))
+    for i in range (len(Bx)):
+        if Scr_x[i]<Vac_xmin:
+            EA_x[i] = 1e6*Scr_x[i]
+        else:
+            EA_x[i] = 1e6*Vac_xmin
+        if Scr_y[i]<Vac_ymin:
+            EA_y[i] = 1e6*Scr_y[i]
+        else:
+            EA_y[i] = 1e6*Vac_ymin
     
 
     #Calcula R
     theta_x = np.zeros(len(Bx))
     theta_y = np.zeros(len(Bx))
-    Bmx = 518/49
-    Bmy = 518/14
+    #Bmx = L/49
+    #Bmy = L/14
+
+    BxL = Bx*length
+    ByL = Bx*length
+    Bmx = sum(BxL)/L
+    Bmy = sum(ByL)/L
+
     for i in range (len(Bx)):
         theta_x[i] = math.sqrt(EA_x[i] /Bmx)
         theta_y[i] = math.sqrt(EA_y[i] /Bmy)
@@ -93,7 +116,6 @@ def elastic(Vx, Vy, Bx, By, c, qe, epsilon0, Kb, Z, E0, T, P):
     F = np.zeros(len(Bx))
     for i in range (len(Bx)):
         F[i] =  (math.pi+(R[i]**2+1)*math.sin(2*math.atan(R[i]))+2*(R[i]**2-1)*math.atan(R[i]))
-    print(2*math.pi/F[5])
     inv_es = Const*Z**2/(E0**2)/T/theta_y**2*F*P
 
     return inv_es[ring_loc]**-1
@@ -109,15 +131,18 @@ def inelastic(accep, P):
 def Touschek(emit, acoplamento, Bx, By, eta_x, spread, accep, sig_S, Ib, T_rev, qe, r0, c, gamma):
 
     #Calcula Touschek
-    Dp = 1e-1
+    
     emitx = emit*(1-acoplamento)
     emity = emit*acoplamento
     V = np.zeros(len(Bx))
     sigx = np.zeros(len(Bx))
+    Dp = np.zeros(len(Bx))
     for i in range (len(Bx)):
         sigx[i] = math.sqrt(emitx*Bx[i]+(eta_x[i]*spread)**2)
         sigy = math.sqrt(emity*By[i])
         V[i] = sig_S * sigx[i] * sigy
+        Dp[i] = Calc_D(accep, sigx[i], gamma)
+
     N = Ib * T_rev / qe 
     inv_T = (r0**2*c/8/math.pi)*N/gamma**2/ accep**3 * Dp / V
 
@@ -125,11 +150,12 @@ def Touschek(emit, acoplamento, Bx, By, eta_x, spread, accep, sig_S, Ib, T_rev, 
 
 def Calc_D(accep, sigx,gamma):
     eps = (accep/(sigx*gamma))**2
-    f1 = math.exp(-x)*math.log(x)/x
-    f2 = math.exp(-x)/x
-    I1 = fixed_quad(f1, eps, 100*eps)
-    I2 = fixed_quad(f2, eps, 100*eps)
-    D = math.sqrt(eps)(-3*math.exp(-eps)/2+eps*I1/2+(3*eps-eps*math.log(eps)+2)*I2
+
+    f1 = lambda x : np.exp(-x)*np.log(x)/x
+    f2 = lambda x : np.exp(-x)/x
+    I1 = integrate.fixed_quad(f1, eps, 100*eps, n=1000)
+    I2 = integrate.fixed_quad(f2, eps, 100*eps, n=1000)
+    D = np.sqrt(eps)*(-3*np.exp(-eps)/2+eps*I1[0]/2+(3*eps-eps*np.log(eps)+2)*I2[0]/2)
     return D
 
 tau = lifetime(model, abertura_x, abertura_y, energy_spread, vac, I, n_bunches, natural_emit, bunch_lengh, coupling, energy_accep)
