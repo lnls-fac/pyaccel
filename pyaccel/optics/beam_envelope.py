4829da8cc96d3be1cd31d385307c3d051dc263ef
ximenes <xresende@gmail.com>
2024-03-26 22:12:41 -0300

    Improve fixed_point property in EpParamsFromBetaEnvelope

--- pyaccel/optics/beam_envelope.py
+++ pyaccel/optics/beam_envelope.py
@@ -123,7 +123,7 @@ class EqParamsFromBeamEnvelope:
     @property
     def fixed_point(self):
         """."""
-        return self._fixed_point.copy()
+        return self._fixed_point.copy() if self._fixed_point is not None else None
 
     @property
     def envelopes(self):
