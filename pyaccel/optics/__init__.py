fe97cc35d44723113673f82dffd7c3ff4edc91ff
JuventinoJFF <75444988+juva143@users.noreply.github.com>
2025-08-20 14:51:03 -0300

    MNT: Complete pyaccel import with optics subpackage

--- pyaccel/optics/__init__.py
+++ pyaccel/optics/__init__.py
@@ -12,3 +12,13 @@ from .miscellaneous import get_chromaticities, get_curlyh, get_frac_tunes, \
     get_rf_frequency, get_rf_voltage, OpticsError
 from .rad_integrals import EqParamsFromRadIntegrals
 from .twiss import calc_twiss, Twiss, TwissArray
+
+__all__ = ['calc_touschek_energy_acceptance', 'calc_transverse_acceptance',
+           'calc_beamenvelope', 'EqParamsFromBeamEnvelope',
+           'FirstOrderDrivingTerms', 'calc_edwards_teng', 'EdwardsTeng',
+           'EdwardsTengArray', 'estimate_coupling_parameters',
+           'EqParamsNormalModes', 'EqParamsXYModes', 'get_chromaticities',
+           'get_curlyh', 'get_frac_tunes', 'get_mcf',
+           'get_revolution_frequency', 'get_revolution_period',
+           'get_rf_frequency', 'get_rf_voltage', 'OpticsError',
+           'EqParamsFromRadIntegrals', 'calc_twiss', 'Twiss', 'TwissArray']
